da3ba773ae753b2097ea15e88519389826d51d75
Haiyan <habuehri@htwg-konstanz.de>
2016-01-20 12:09:35 +0100

    default value for create new game

--- GoBang_Web/public/javascripts/main.js
+++ GoBang_Web/public/javascripts/main.js
@@ -1,17 +1,29 @@
 $(function() {
 	$('#playGame').click(function() {
+		if(document.getElementById("playName").value == "") {
+			document.getElementById("playName").value = "NewPlayGame";
+		}
 		window.location.replace('/game/' + document.getElementById("playName").value)
 	});
 	
 	$('#ngGame').click(function() {
+		if(document.getElementById("ngName").value == "") {
+			document.getElementById("ngName").value = "NewAngularGame";
+		}
 		window.location.replace('/nggame/' + document.getElementById("ngName").value)
 	});
 	
 	$('#createPlay').click(function() {
+		if(document.getElementById("createPlayName").value == "") {
+			document.getElementById("createPlayName").value = "NewPlayGame";
+		}
 		window.location.replace('/game/' + document.getElementById("createPlayName").value)
 	});
 	
 	$('#createNGJS').click(function() {
+		if(document.getElementById("createNGJSName").value == "") {
+			document.getElementById("createNGJSName").value = "NewAngularGame";
+		}
 		window.location.replace('/nggame/' + document.getElementById("createNGJSName").value)
 	});
 });
